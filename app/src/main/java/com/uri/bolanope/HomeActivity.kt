28082fa42decb283fffef7b30402d9a8d0b21c60
otaviocosta2110 <otaviocosta23212@gmail.com>
2024-09-12 15:02:26 -0300

    added field on homepage

--- app/src/main/java/com/uri/bolanope/HomeActivity.kt
+++ app/src/main/java/com/uri/bolanope/HomeActivity.kt
@@ -1,25 +1,43 @@
 package com.uri.bolanope
 
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Scaffold
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
 import com.uri.bolanope.utils.SharedPreferencesManager
 
 @Composable
 fun HomePage(navController: NavHostController) {
     val context = LocalContext.current
     val userId = SharedPreferencesManager.getUserId(context)
+    val imageList = listOf(
+        R.drawable.quadra1
+    )
+    val pagerState = rememberPagerState(pageCount = {imageList.size})
+    val coroutineScope = rememberCoroutineScope()
 
-    Scaffold (
+    Scaffold(
         bottomBar = { BottomNavigationBar(navController) },
         modifier = Modifier.fillMaxSize()
     ) { innerPadding ->
@@ -27,12 +45,47 @@ fun HomePage(navController: NavHostController) {
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
-                .padding(16.dp)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = "Home",
-                modifier = Modifier.align(Alignment.CenterHorizontally)
+            Image(
+                painter = painterResource(id = R.drawable.ic_logo_bolanope),
+                contentDescription = "Logo Bola no Pé",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .padding(top = 16.dp)
+                    .size(150.dp)
             )
+            Text(
+                text = "Nossas quadras:",
+                modifier = Modifier
+                    .padding(top = 16.dp, start = 8.dp)
+                    .align(Alignment.Start)
+            )
+            HorizontalPager(
+                state = pagerState,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp) // Adjust height as needed
+            ) { page ->
+                Card(
+                    shape = RoundedCornerShape(8.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = imageList[page]),
+                        contentDescription = "Image $page",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxSize()
+                            .clickable {
+                                navController.navigate("user")
+                            }
+                    )
+                }
+            }
+
         }
     }
 }
