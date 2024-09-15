cedd6c626092b5d7ff67997ff781f8c9d63ebf67
otaviocosta2110 <otaviocosta23212@gmail.com>
2024-09-15 16:24:47 -0300

    date, cpf and cep on register

---
+++ app/src/main/java/com/uri/bolanope/Mask.kt
@@ -0,0 +1,44 @@
+package com.uri.bolanope
+
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.input.OffsetMapping
+import androidx.compose.ui.text.input.TransformedText
+import androidx.compose.ui.text.input.VisualTransformation
+import kotlin.math.absoluteValue
+
+// nao achei um jeito melhor de fazer isso se nao fazendo uma classe nova (acho que n sou mt esperto)
+class MaskVisualTransformation(private val mask: String) : VisualTransformation {
+
+    private val specialSymbolsIndices = mask.indices.filter { mask[it] != '#' }
+
+    override fun filter(text: AnnotatedString): TransformedText {
+        var out = ""
+        var maskIndex = 0
+        text.forEach { char ->
+            while (specialSymbolsIndices.contains(maskIndex)) {
+                out += mask[maskIndex]
+                maskIndex++
+            }
+            out += char
+            maskIndex++
+        }
+        return TransformedText(AnnotatedString(out), offsetTranslator())
+    }
+
+    private fun offsetTranslator() = object : OffsetMapping {
+        override fun originalToTransformed(offset: Int): Int {
+            val offsetValue = offset.absoluteValue
+            if (offsetValue == 0) return 0
+            var numberOfHashtags = 0
+            val masked = mask.takeWhile {
+                if (it == '#') numberOfHashtags++
+                numberOfHashtags < offsetValue
+            }
+            return masked.length + 1
+        }
+
+        override fun transformedToOriginal(offset: Int): Int {
+            return mask.take(offset.absoluteValue).count { it == '#' }
+        }
+    }
+}
--- app/src/main/java/com/uri/bolanope/WelcomeActivity.kt
+++ app/src/main/java/com/uri/bolanope/WelcomeActivity.kt
@@ -16,50 +16,57 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.uri.bolanope.utils.SharedPreferencesManager
 
 @Composable
 fun Welcome(navController: NavHostController) {
-
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-
-        Image(
-            painter = painterResource(id = R.drawable.ic_logo_bolanope),
-            contentDescription = "Logo Bola no Pé",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .align(Alignment.TopCenter)
-                .padding(top = 200.dp)
-                .size(150.dp)
-        )
-
-        Column(
+    val context = LocalContext.current
+//    val userId = SharedPreferencesManager.getUserId(context)
+//    if (!userId.isNullOrEmpty()) {
+//        navController.navigate("home")
+//    } else {
+        Box(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
         ) {
-            Button(onClick = { navController.navigate("login") }) {
-                Text("Entrar")
-            }
 
-            Spacer(modifier = Modifier.height(16.dp))
-
-            Text(
-                text = "Não possui conta? Clique aqui e cadastre-se",
-                style = TextStyle(fontSize = 16.sp),
+            Image(
+                painter = painterResource(id = R.drawable.ic_logo_bolanope),
+                contentDescription = "Logo Bola no Pé",
+                contentScale = ContentScale.Crop,
                 modifier = Modifier
-                    .clickable { navController.navigate("user") }
-                    .padding(8.dp)
+                    .align(Alignment.TopCenter)
+                    .padding(top = 200.dp)
+                    .size(150.dp)
             )
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Button(onClick = { navController.navigate("login") }) {
+                    Text("Entrar")
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Text(
+                    text = "Não possui conta? Clique aqui e cadastre-se",
+                    style = TextStyle(fontSize = 16.sp),
+                    modifier = Modifier
+                        .clickable { navController.navigate("user") }
+                        .padding(8.dp)
+                )
+            }
         }
-    }
+//    }
 }
